5d00da8247914d69b2f558628b7896e4212bb049
Ali Haider <ahwebdeveloper856@gmail.com>
2026-07-23 18:01:00 -0400

    Added error handling

--- emotion_detection.py
+++ emotion_detection.py
@@ -21,6 +21,15 @@ def emotion_detector(text_to_analyze):
         json=input_json,
         headers=headers
     )
+    if response.status_code == 400:
+    return {
+        "anger": None,
+        "disgust": None,
+        "fear": None,
+        "joy": None,
+        "sadness": None,
+        "dominant_emotion": None
+    }
 
     response_data = json.loads(response.text)
 
